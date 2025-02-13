735e1ebf2e54759a641175ba6194d952f3cf905c
SG-WORK <gpsara1114@gmail.com>
2025-02-12 18:51:23 -0600

    Creacion de menus mediante la navegacion del menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-btn");
 const sidebar = document.getElementById("sidebar");
+const menuItems = document.querySelectorAll(".menu-item");
+const sections = document.querySelectorAll(".content-section");
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -16,5 +19,41 @@ document.addEventListener("DOMContentLoaded", function () {
     sidebar.classList.add("-translate-x-full");
   });
 
+  // Evento para los elementos del menu
+  const activeItem = document.querySelector(".menu-item.active") || menuItems[0];
+  if(activeItem) updateIndicator(activeItem)
+  
+  // Evento click en cada item
+  menuItems.forEach((item) => {
+      item.addEventListener("click", function (e) {
+      e.preventDefault();
+      menuItems.forEach((el) => el.classList.remove("active"));
+      this.classList.add("active");
+      updateIndicator(this);
+
+      // Obtener la sección correspondiente
+      const targetSection = this.getAttribute("data-section");
+
+      // Ocultar todas las secciones
+      sections.forEach(section => section.classList.add("hidden"));
+
+      // Mostrar la sección seleccionada
+      document.getElementById(targetSection).classList.remove("hidden");
+
+      // Remover la clase 'active' de todos los botones
+      menuItems.forEach(btn => btn.classList.remove("active"));
+      
+      // Agregar la clase 'active' al botón seleccionado
+      this.classList.add("active");
+      
+      });
+  });
+
+
+  function updateIndicator(activeItem) {
+    const { offsetLeft, offsetWidth } = activeItem;
+    indicator.style.width = `${offsetWidth}px`;
+    indicator.style.transform = `translateX(${offsetLeft}px)`;
+  }
 });
   
